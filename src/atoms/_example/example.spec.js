9b6d387b1f55caec3d8f3a48eb6400fbc8cd4b45
Clayton <flexgh.us@gmail.com>
2018-10-16 11:09:05 -0300

    :ant: include scripts to update tests and verify coverage

--- src/atoms/_example/example.spec.js
+++ src/atoms/_example/example.spec.js
@@ -6,12 +6,20 @@ it('should render a component', () => {
   const wrapper = shallow(
     <Component />,
   ).dive();
+  expect(wrapper.props().color).toEqual('tomato');
   expect(wrapper).toMatchSnapshot();
 });
 
 it('should render a component with a prop', () => {
   const wrapper = shallow(
     <Component strong />,
+  ).dive();
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should render a component with a color', () => {
+  const wrapper = shallow(
+    <Component color="black" />,
   ).dive();
   expect(wrapper).toMatchSnapshot();
 });
