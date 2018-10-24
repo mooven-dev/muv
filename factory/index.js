8cdee6612299a1ff9d821c2ca61d2499ad372247
Clayton <flexgh.us@gmail.com>
2018-10-24 13:15:32 -0300

    :ant: rule to avoid duplicated component names, autoupdate documentation

--- factory/index.js
+++ factory/index.js
@@ -2,15 +2,17 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
-
 // DEFINE
 const crateNewComponent = (args) => {
   // SETUP
   const type = args[2];
   const { log } = console;
   const component = args[3];
-  const nameList = [];
   const typeList = ['atom', 'molecule', 'organism', 'template'];
+  const nameListPath = './factory/components.txt';
+  let nameList = fs.readFileSync(nameListPath, 'utf8');
+  nameList = nameList.replace(/'/g, '"');
+  nameList = JSON.parse(nameList);
 
   // help message
   const helpMessage = (cmderr = '') => {
@@ -42,15 +44,21 @@ const crateNewComponent = (args) => {
   }
 
   // component name must be unique
-  if (nameList.filter(item => item === type).length) {
+  if (nameList.filter(item => item === component).length) {
     return helpMessage('component name must be unique');
   }
+
+  // save component name on list
+  nameList.push(component);
+  nameList = JSON.stringify(nameList);
+  fs.writeFileSync(nameListPath, nameList, 'utf-8');
 
   // PREPARE
   const exportPath = 'src/index.js';
   const modelPath = 'factory/model';
-  const destFolder = `src/${type}s`;
-  const componentPath = `${destFolder}/${component}`;
+  const destPath = `src/${type}s`;
+  const documentPath = 'src/index.mdx';
+  const componentPath = `${destPath}/${component}`;
 
   // log messages
   const success = message => log(chalk.bgGreen.bold(' SUCCESS '), chalk.green(message));
@@ -59,8 +67,8 @@ const crateNewComponent = (args) => {
   // CMD
   return fs.mkdir(componentPath, {}, (errmk) => {
     // create directory
-    if (errmk) return error(`Error creating diretory ${chalk.bold(component)} on ${destFolder}`, errmk);
-    success(`${chalk.bold(component)} folder created on ${destFolder}`, '✓');
+    if (errmk) return error(`Error creating diretory ${chalk.bold(component)} on ${destPath}`, errmk);
+    success(`${chalk.bold(component)} folder created on ${destPath}`, '✓');
     fs.readdir(modelPath, (errread, files) => {
       // read files from model
       if (errread) return error(`Error reading directory ${chalk.bold(component)}`, errread);
@@ -95,7 +103,26 @@ const crateNewComponent = (args) => {
       // add new content on export file
       fs.writeFile(exportPath, withExport, 'utf-8', (writeerr) => {
         if (writeerr) return error(`Exporting ${component} to module`, readerr);
-        return true;
+        return success(`${Component} exported on ${exportPath}`);
+      });
+      return true;
+    });
+
+    // read index documentation
+    fs.readFile(documentPath, 'utf-8', (readerr, content) => {
+      if (readerr) return error(`Exporting ${component} to module`, readerr);
+      const Component = component.charAt(0).toUpperCase() + component.slice(1);
+      const TYPE = type.toUpperCase();
+      // edit content to import file on right list
+      const withDoc = content.replace(
+        `## ${TYPE}S`,
+        `## ${TYPE}S\n- [${Component}](/${component})`,
+      );
+
+      // add new content on export file
+      fs.writeFile(documentPath, withDoc, 'utf-8', (writeerr) => {
+        if (writeerr) return error(`Writing ${component} on documentation index`, readerr);
+        return success(`${Component} included on ${documentPath}`);
       });
       return true;
     });
