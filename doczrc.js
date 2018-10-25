6f3234f7129acaba7412b8e5f1a0a07cd16f8264
Clayton <flexgh.us@gmail.com>
2018-10-25 13:08:41 -0300

    :ant: better theming, documentation theme

---
+++ doczrc.js
@@ -0,0 +1,28 @@
+
+import theme from './src/theme';
+
+export default {
+  title: 'MUV',
+  port: '4000',
+  dest: '/docz',
+  description: 'This is my awesome documentation',
+  themeConfig: {
+    colors: {
+      background: theme.color.white,
+      sidebarText: theme.color.black,
+      secondary: theme.color.secondary,
+      primary: theme.color.primary,
+      theadColor: theme.text.color,
+      sidebarBg: theme.color.gray,
+      link: theme.color.primary,
+      theadBg: theme.color.gray,
+      border: theme.color.gray,
+      text: theme.text.color,
+    },
+  },
+  styles: {
+    body: {
+      fontSize: theme.shape.base,
+    },
+  },
+};
--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,34 @@
+const color = {
+  overlay: 'rgba(0, 0, 0, .15)',
+  secondary: '#00baf2',
+  primary: '#0082c8',
+  white: '#fefefe',
+  black: '#484848',
+  warn: '#dd3333',
+  gray: '#f0f0f0',
+};
+
+const shape = {
+  shadow: `0 .25rem .25rem 0 ${color.overlay}`,
+  border: `.125rem solid ${color.primary}`,
+  padding: '.825rem 1.2rem',
+  margin: '.25rem 0',
+  radius: '.25rem',
+  base: '1rem',
+};
+
+const text = {
+  color: color.black,
+  size: shape.base,
+};
+
+const transition = {
+  time: '.2s',
+};
+
 export default {
-  shape: {
-    shadow: '0 4px 5px 0 rgba(0,0,0,0.15)',
-    border: '0 solid transparent',
-    padding: '.8rem 1.2rem',
-    margin: '.25rem',
-    radius: '4px',
-  },
-  color: {
-    secondary: '#00baf2',
-    primary: '#0082c8',
-    white: '#fefefe',
-  },
-  transition: {
-    time: '.2s',
-  },
+  transition,
+  shape,
+  color,
+  text,
 };
