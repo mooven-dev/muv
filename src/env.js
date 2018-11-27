d72c44caa613b1eeca3a97a20a7106367527da2c
Clayton <flexgh.us@gmail.com>
2018-11-27 17:14:34 -0200

    :100: bot

--- src/env.js
+++ src/env.js
@@ -4,19 +4,19 @@ const { NODE_ENV } = process.env;
 const config = {
   env: NODE_ENV || 'development',
   development: {
-    conversation: 'https://hyundai-watson.now.sh/api/conversation',
+    conversation_api: 'https://hyundai-watson.now.sh/api/conversation',
     listWorkspaces: 'https://hyundai-watson.now.sh/api/list-workspaces',
     // conversation: 'http://localhost:4001',
     // listWorkspaces: 'http://localhost:4002',
     workspace_id: '60b42e0e-a820-4e6a-8075-c28a40766072',
   },
   test: {
-    conversation: 'https://hyundai-watson.now.sh/api/conversation',
+    conversation_api: 'https://hyundai-watson.now.sh/api/conversation',
     listWorkspaces: 'https://hyundai-watson.now.sh/api/list-workspaces',
     workspace_id: '60b42e0e-a820-4e6a-8075-c28a40766072',
   },
   production: {
-    conversation: 'https://hyundai-watson.now.sh/api/conversation',
+    conversation_api: 'https://hyundai-watson.now.sh/api/conversation',
     listWorkspaces: 'https://hyundai-watson.now.sh/api/list-workspaces',
     workspace_id: '60b42e0e-a820-4e6a-8075-c28a40766072',
   },
--- src/organisms/chatbot/index.js
+++ src/organisms/chatbot/index.js
@@ -1,5 +1,5 @@
 // IMPORTS
-import { node, number } from 'prop-types';
+import { node, number, string } from 'prop-types';
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Axios from 'axios';
@@ -18,7 +18,7 @@ import Fab from '../../atoms/fab';
 import Col from '../../atoms/col';
 import config from '../../env';
 
-const { conversation, workspace_id } = config;
+const { conversation_api, workspace_id } = config;
 
 // STYLES
 const ChatBox = styled(Container)`
@@ -82,8 +82,10 @@ class Chatbot extends Component {
       messages: [],
       open: true,
     };
+
     // REF TO SCROLL CHAT WINDOW ON NEW MESSAGE
     this.scrollRef = React.createRef();
+
     // UPDATE CHAT SCREEEN
     this.updateChat = (message) => {
       const { messages } = this.state;
@@ -92,21 +94,29 @@ class Chatbot extends Component {
       if (message.output === undefined) disabled = true;
       this.setState({ messages: newMessages, text: '', isLoad: true, disabled });
     };
+
     // SEND MESSAGE
     this.messageRequest = (data) => {
+      const { endPoint } = this.props;
       this.updateChat(data);
-      Axios.post(conversation, JSON.stringify(data))
+      Axios.post(endPoint, JSON.stringify(data))
         .then((res) => {
           this.updateChat(res.data);
         })
         .catch(err => console.log('error', err)); // eslint-disable-line
     };
+
+    // CLOSE BOT
     // eslint-disable-next-line
     this.close = () => this.setState({ open: false, willClose: '', newMessages: 2 });
+
+    // PREPARE AUTOMATIC CLOSE
     this.prepareToClose = () => {
       // eslint-disable-next-line
       this.setState({ secondWarn: '', willClose: setTimeout(this.close, this.props.timeOut) });
     };
+
+    // WARN TO INACTIVE USER (SECOND TRY)
     this.startSecondWarn = (data) => {
       const sendSecondWarn = () => {
         const newData = data;
@@ -117,6 +127,8 @@ class Chatbot extends Component {
       // eslint-disable-next-line
       this.setState({ firstWarn: '', secondWarn: setTimeout(sendSecondWarn, this.props.timeOut) });
     };
+
+    // WARN TO INACTIVE USER (FIRST TRY)
     this.startFirstWarn = (data) => {
       const sendFirstWarn = () => {
         const newData = data;
@@ -128,7 +140,8 @@ class Chatbot extends Component {
       // eslint-disable-next-line
       this.setState({ firstWarn: setTimeout(sendFirstWarn, this.props.timeOut) });
     };
-    // SEND MESSAGES
+
+    // SEND USER MESSAGES TO BOT
     this.sendMessage = (message) => {
       // eslint-disable-next-line
       this.setState({ disabled: true, firstWarn: '', secondWarn: '', willClose: '' });
@@ -136,12 +149,13 @@ class Chatbot extends Component {
       const automatic = (message === undefined);
       const text = (message || 'ola');
       // GET CONTEXT
+      const { workSpace } = this.props;
       const { messages } = this.state;
       const lastMessage = (messages.length && messages[messages.length - 1]);
       const { context } = lastMessage;
       // SETUP
       const data = {
-        workspace_id,
+        workspace_id: workSpace,
         automatic,
         context,
         input: {
@@ -151,19 +165,24 @@ class Chatbot extends Component {
       this.startFirstWarn(data);
       this.messageRequest(data);
     };
-    // OPENS AND CLOSES CHAT WINDOW
+
+    // OPENS AND CLOSES CHAT WINDOW (USUALLY ON UI)
     this.toogleChat = () => this.setState(({ open }) => ({ open: !open, newMessages: 0 }));
+
     // LOOKS FOR NEW MESSAGES AND SCROLL CHAT WINDOW
     this.scrollOnNewMessage = () => {
       const box = this.scrollRef.current;
       if (box) box.scrollIntoView({ behavior: 'smooth', block: 'end' });
     };
+
     // SEND USER MESSAGES TO BOT AND HANDLES HIS RESPONSE
     this.submitMessage = (e) => {
       if (e) e.preventDefault();
       const { text } = this.state;
       this.sendMessage(text);
     };
+
+    // UPDATES NEW MESSAGES BADGE
     this.setNotification = () => {
       const { open } = this.state;
       if (open) this.setState({ newMessages: 0 });
@@ -172,10 +191,12 @@ class Chatbot extends Component {
   }
 
   componentDidMount() {
+    // START CONVERSATION
     this.sendMessage();
   }
 
   componentDidUpdate(prevPros, prevState) {
+    // LOOKS FOR NEW MESSAGES
     const newMessages = this.state.messages;
     const oldMessages = prevState.messages;
     if (newMessages.length > oldMessages.length) {
@@ -186,24 +207,26 @@ class Chatbot extends Component {
 
   render() {
     const { open, messages, isLoad, disabled, newMessages } = this.state;
-    const { children, ...messageProps } = this.props;
+    const { children, botName, ...messageProps } = this.props;
     return (
       <>
+        {/* BOT BUTTON */}
         <Fab onClick={this.toogleChat}>
           <span>
             <Badge>{Math.floor(newMessages / 2)}</Badge>
             {children}
           </span>
         </Fab>
+        {/* BOT WINDOW */}
         <ChatBox flex open={(isLoad && open)}>
           {/* HEADER */}
           <Header secondary grow={0} hasContent bordered justify="space-between">
-            <Text white strong>Chatbot</Text>
+            <Text white strong>{botName}</Text>
             <Link white noLine onClick={this.toogleChat}>
               <Icon color="white" name="close" />
             </Link>
           </Header>
-          {/* BODY */}
+          {/* BODY (MESSAGES GOES HERE) */}
           <Body open={open}>
             <div ref={this.scrollRef}>
               {
@@ -248,13 +271,23 @@ class Chatbot extends Component {
 
 // DOCUMENTATION
 Chatbot.propTypes = {
+  /** path to the conversation api */
+  endPoint: string,
+  /** the dialog workspace_id */
+  workSpace: string,
+  /** the name displayed on the box screen header */
+  botName: string,
+  /** sets the user inactivity threshold */
   timeOut: number,
-  /** accepts only valid react nodes as children */
+  /** the icon, component, or image rendered inside the bot button */
   children: node,
 };
 
 Chatbot.defaultProps = {
   children: <Icon fontSize="2rem" name="contacts" color="white" />,
+  endPoint: conversation_api,
+  botName: 'Mooven ChatBot',
+  workSpace: workspace_id,
   timeOut: 60000,
 };
 
